fbb6f89b63388b84b8b574813ffd7d52fec81ea7
Davide Sforza <sforza.1710262@studenti.uniroma1.it>
2020-06-25 19:22:19 +0200

    Update

--- src/ata.cu
+++ src/ata.cu
@@ -1,54 +1,49 @@
 #include "strassen.cu"
 
-/*
-  lda, ldb, ldc is the width in actual memory.
-  XA, XB, XC is the width for computation.
-  A = XA x YA
-  B = XB x YB
-  C = XC x YC
-*/
-void ata(double *A, double *B, double *C,
+void GPU_trans(double *A, double *B, double *C,
     int lda, int ldb, int ldc,
-    int XA, int XB, int XC,
-    int YA, int YB, int YC,
+    int XA, int YA,
+    double alpha, double beta) {
+  cublasDgeam(handle, CUBLAS_OP_N, CUBLAS_OP_N, XA, YA, &alpha, A, lda, &beta, B, ldb, C, ldc);
+}
+
+/*
+  lda is the width in actual memory.
+  XA is the width for computation.
+  A = XA x YA
+  C = XA x XA
+*/
+void ata(double *A, double *C,
+    int lda, int ldc,
+    int XA, int YA,
     int depth) {
 
   int XA2 = XA / 2;
-  int XB2 = XB / 2;
-  int XC2 = XC / 2;
-  
   int YA2 = YA / 2;
-  int YB2 = YB / 2;
-  int YC2 = YC / 2;
+
+  int XC2 = XA2;
+  int YC2 = YA2;
 
   double *W_1, *W_2;
-  int lw1 = (XA2 > XC2 ? XA2 : XC2);
-  int lw2 = XB2;
+  int lw1 = XA2;
+  int lw2 = YA2;
   cudaMalloc((void **)&W_1, lw1 * YA2 * sizeof(double));
   cudaMalloc((void **)&W_2, lw2 * YB2 * sizeof(double));
 
   int dXA = XA2;
   int dYA = YA2 * lda;
-  int dXB = XB2;
-  
-  int dYB = YB2 * ldb;
+
   int dXC = XC2;
   int dYC = YC2 * ldc;
 
   double *A11, *A12, *A21, *A22;
-  double *B11, *B12, *B21, *B22;
   double *C11, *C12, *C21, *C22;
   
   A11 = A;
   A12 = A + dXA;
   A21 = A + dYA;
   A22 = A + dXA + dYA;
-  
-  B11 = B;
-  B12 = B + dXB;
-  B21 = B + dYB;
-  B22 = B + dXB + dYB;
-  
+
   C11 = C;
   C12 = C + dXC;
   C21 = C + dYC;
@@ -92,28 +87,17 @@ void ata(double *A, double *B, double *C,
     GPU_add(W_1, C11, C11, lw1, ldc, ldc, XC2, YC2, 1.0,  1.0); // C11 = W_1+ C11
   }
   else {
-    GPU_add(A11, A21, W_1, lda, lda, lw1, XA2, YA2, 1.0, -1.0); // W_1 = A11 - A21
-    GPU_add(B22, B12, W_2, ldb, ldb, lw2, XB2, YB2, 1.0, -1.0); // W_2 = B22 - B12
-    strassen(W_1, W_2, C21, lw1, lw2, ldc, XA2, XB2, XC2, YA2, YB2, YC2, depth - 1);
-    GPU_add(A21, A22, W_1, lda, lda, lw1, XA2, YA2, 1.0,  1.0); // W_1 = A21 + A22
-    GPU_add(B12, B11, W_2, ldb, ldb, lw2, XB2, YB2, 1.0, -1.0); // W_2 = B12 - B11
-    strassen(W_1, W_2, C22, lw1, lw2, ldc, XA2, XB2, XC2, YA2, YB2, YC2, depth - 1);
-    GPU_add(W_1, A11, W_1, lw1, lda, lw1, XA2, YA2, 1.0, -1.0); // W_1 = W_1- A11
-    GPU_add(B22, W_2, W_2, ldb, lw2, lw2, XB2, YB2, 1.0, -1.0); // W_2 = B22 - W_2
-    strassen(W_1, W_2, C11, lw1, lw2, ldc, XA2, XB2, XC2, YA2, YB2, YC2, depth - 1);
-    GPU_add(A12, W_1, W_1, lda, lw1, lw1, XA2, YA2, 1.0, -1.0); // W_1 = A12 - W_1
-    strassen(W_1, B22, C12, lw1, ldb, ldc, XA2, XB2, XC2, YA2, YB2, YC2, depth - 1);
-    GPU_add(C22, C12, C12, ldc, ldc, ldc, XC2, YC2, 1.0,  1.0); // C12 = C22 + C12
-    strassen(A11, B11, W_1, lda, ldb, lw1, XA2, XB2, XC2, YA2, YB2, YC2, depth - 1);
-    GPU_add(W_1, C11, C11, lw1, ldc, ldc, XC2, YC2, 1.0,  1.0); // C11 = W_1 + C11
-    GPU_add(C11, C12, C12, ldc, ldc, ldc, XC2, YC2, 1.0,  1.0); // C12 = C11 + C12
-    GPU_add(C11, C21, C11, ldc, ldc, ldc, XC2, YC2, 1.0,  1.0); // C11 = C11 + C21
-    GPU_add(W_2, B21, W_2, lw2, ldb, lw2, XB2, YB2, 1.0, -1.0); // W_2 = W_2- B21
-    strassen(A22, W_2, C21, lda, lw2, ldc, XA2, XB2, XC2, YA2, YB2, YC2, depth - 1);
-    GPU_add(C11, C21, C21, ldc, ldc, ldc, XC2, YC2, 1.0, -1.0); // C11 = C11 - C21
-    GPU_add(C11, C22, C22, ldc, ldc, ldc, XC2, YC2, 1.0,  1.0); // C22 = C11 + C22
-    strassen(A12, B21, C11, lda, ldb, ldc, XA2, XB2, XC2, YA2, YB2, YC2, depth - 1);
-    GPU_add(W_1, C11, C11, lw1, ldc, ldc, XC2, YC2, 1.0,  1.0); // C11 = W_1+ C11
+    ata(A11, W_1, lda, ldc, XA2, YA2, depth - 1);  // S1 = ata(A11)
+    ata(A21, W_2, lda, ldc, XA2, YA2, depth - 1);  // S2 = ata(A21)
+    GPU_add(C11, A11, W_1, lw1, lda, lw1, XA2, YA2, 1.0, -1.0);  // C11 = S1 + S2
+    ata(W_1, W_2, C11, lw1, lw2, ldc, XA2, XB2, XC2, YA2, YB2, YC2, depth - 1);  // S3 = ata(A12)
+    ata(W_1, B22, C12, lw1, ldb, ldc, XA2, XB2, XC2, YA2, YB2, YC2, depth - 1);  // S4 = ata(A22)
+    GPU_add(C22, C12, C12, ldc, ldc, ldc, XC2, YC2, 1.0,  1.0);  // C22 = S3 + S4
+    GPU_trans(C11, C22, C22, ldc, ldc, ldc, XC2, YC2, 1.0,  1.0);  // A12_t
+    strassen(A22, W_2, C21, lda, lw2, ldc, XA2, XB2, XC2, YA2, YB2, YC2, depth - 1);  // S5 = strassen(A12_t, A11)
+    GPU_trans(C11, C22, C22, ldc, ldc, ldc, XC2, YC2, 1.0,  1.0);  // A22_t
+    strassen(A12, B21, C11, lda, ldb, ldc, XA2, XB2, XC2, YA2, YB2, YC2, depth - 1);  // S6 = strassen(A22_t, A21)
+    GPU_add(W_1, C11, C11, lw1, ldc, ldc, XC2, YC2, 1.0,  1.0);  // C21 = S5 + S6
   }
   cudaFree(W_1);
   cudaFree(W_2);
