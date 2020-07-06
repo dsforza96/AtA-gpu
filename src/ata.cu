479bb0f01e4c1cf33418b88c4da4353d46bf6967
Davide Sforza <sforza.1710262@studenti.uniroma1.it>
2020-07-06 17:09:58 +0200

    Update. Missing dynamic peeling

--- src/ata.cu
+++ src/ata.cu
@@ -1,34 +1,49 @@
 #include "strassen.cu"
 
-void GPU_trans(double *A, double *B, double *C,
-    int lda, int ldb, int ldc,
-    int XA, int YA,
-    double alpha, double beta) {
-  cublasDgeam(handle, CUBLAS_OP_N, CUBLAS_OP_N, XA, YA, &alpha, A, lda, &beta, B, ldb, C, ldc);
+void GPU_trans(double *A, double *C,
+    int lda, int ldc,
+    int XA, int YA) {
+  double one = 1.0;
+  double zero = 0.0;
+  cublasDgeam(handle, CUBLAS_OP_T, CUBLAS_OP_T, XA, YA, &one, A, lda, &zero, A, lda, C, ldc);
+}
+
+void GPU_ata(double *A, double *C, int M, int N) {
+  double one = 1.0;
+  double zero = 0.0;
+#if CMAJOR
+  cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, M, N, M, &one, A, M, A, M, &zero, C, M);
+#else
+  cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, N, M, M, &one, A, N, A, M, &zero, C, N);
+#endif
 }
 
 /*
-  lda is the width in actual memory.
+  lda, ldc is the width in actual memory.
   XA is the width for computation.
   A = XA x YA
   C = XA x XA
 */
 void ata(double *A, double *C,
     int lda, int ldc,
-    int XA, int YA,
+    int XA, int XC,
+    int YA, int YC,
     int depth) {
 
   int XA2 = XA / 2;
-  int YA2 = YA / 2;
+  int XC2 = XC / 2;
 
-  int XC2 = XA2;
-  int YC2 = YA2;
+  int YA2 = YA / 2;
+  int YC2 = YC / 2;
 
   double *W_1, *W_2;
+  double *A12_t, *A22_t;
   int lw1 = XA2;
-  int lw2 = YA2;
+  int lw2 = XA2;
   cudaMalloc((void **)&W_1, lw1 * YA2 * sizeof(double));
-  cudaMalloc((void **)&W_2, lw2 * YB2 * sizeof(double));
+  cudaMalloc((void **)&W_2, lw2 * YA2 * sizeof(double));
+  cudaMalloc((void **)&A12_t, YA2 * XA2 * sizeof(double));
+  cudaMalloc((void **)&A22_t, YA2 * XA2 * sizeof(double));
 
   int dXA = XA2;
   int dYA = YA2 * lda;
@@ -63,44 +78,25 @@ void ata(double *A, double *C,
 #endif
 
   if (depth <= 1 || stop) {
-    GPU_add(A11, A21, W_1, lda, lda, lw1, XA2, YA2, 1.0, -1.0); // W_1 = A11 - A21
-    GPU_add(B22, B12, W_2, ldb, ldb, lw2, XB2, YB2, 1.0, -1.0); // W_2 = B22 - B12
-    GPU_mul(W_1, W_2, C21, lw1, lw2, ldc, XA2, XB2, XC2, YA2, YB2, YC2, 1.0, 0.0); // C21 = W_1 * W_2
-    GPU_add(A21, A22, W_1, lda, lda, lw1, XA2, YA2, 1.0,  1.0); // W_1 = A21 + A22
-    GPU_add(B12, B11, W_2, ldb, ldb, lw2, XB2, YB2, 1.0, -1.0); // W_2 = B12 - B11
-    GPU_mul(W_1, W_2, C22, lw1, lw2, ldc, XA2, XB2, XC2, YA2, YB2, YC2, 1.0, 0.0); // C22 = W_1 * W_2
-    GPU_add(W_1, A11, W_1, lw1, lda, lw1, XA2, YA2, 1.0, -1.0); // W_1 = W_1- A11
-    GPU_add(B22, W_2, W_2, ldb, lw2, lw2, XB2, YB2, 1.0, -1.0); // W_2 = B22 - W_2
-    GPU_mul(W_1, W_2, C11, lw1, lw2, ldc, XA2, XB2, XC2, YA2, YB2, YC2, 1.0, 0.0); // C11 = W_1 * W_2
-    GPU_add(A12, W_1, W_1, lda, lw1, lw1, XA2, YA2, 1.0, -1.0); // W_1 = A12 - W_1
-    GPU_mul(W_1, B22, C12, lw1, ldb, ldc, XA2, XB2, XC2, YA2, YB2, YC2, 1.0, 0.0); // C12 = W_1 * B22
-    GPU_add(C22, C12, C12, ldc, ldc, ldc, XC2, YC2, 1.0,  1.0); // C12 = C22 + C12
-    GPU_mul(A11, B11, W_1, lda, ldb, lw1, XA2, XB2, XC2, YA2, YB2, YC2, 1.0, 0.0); // W_1= A11 * B11
-    GPU_add(W_1, C11, C11, lw1, ldc, ldc, XC2, YC2, 1.0,  1.0); // C11 = W_1 + C11
-    GPU_add(C11, C12, C12, ldc, ldc, ldc, XC2, YC2, 1.0,  1.0); // C12 = C11 + C12
-    GPU_add(C11, C21, C11, ldc, ldc, ldc, XC2, YC2, 1.0,  1.0); // C11 = C11 + C21
-    GPU_add(W_2, B21, W_2, lw2, ldb, lw2, XB2, YB2, 1.0, -1.0); // W_2 = W_2- B21
-    GPU_mul(A22, W_2, C21, lda, lw2, ldc, XA2, XB2, XC2, YA2, YB2, YC2, 1.0, 0.0); // C21 = A22 * W_2
-    GPU_add(C11, C21, C21, ldc, ldc, ldc, XC2, YC2, 1.0, -1.0); // C11 = C11 - C21
-    GPU_add(C11, C22, C22, ldc, ldc, ldc, XC2, YC2, 1.0,  1.0); // C22 = C11 + C22
-    GPU_mul(A12, B21, C11, lda, ldb, ldc, XA2, XB2, XC2, YA2, YB2, YC2, 1.0, 0.0); // C11 = A12 * B21
-    GPU_add(W_1, C11, C11, lw1, ldc, ldc, XC2, YC2, 1.0,  1.0); // C11 = W_1+ C11
+    GPU_ata(A, C, XA, YA);
   }
   else {
-    ata(A11, W_1, lda, ldc, XA2, YA2, depth - 1);  // S1 = ata(A11)
-    ata(A21, W_2, lda, ldc, XA2, YA2, depth - 1);  // S2 = ata(A21)
-    GPU_add(C11, A11, W_1, lw1, lda, lw1, XA2, YA2, 1.0, -1.0);  // C11 = S1 + S2
-    ata(W_1, W_2, C11, lw1, lw2, ldc, XA2, XB2, XC2, YA2, YB2, YC2, depth - 1);  // S3 = ata(A12)
-    ata(W_1, B22, C12, lw1, ldb, ldc, XA2, XB2, XC2, YA2, YB2, YC2, depth - 1);  // S4 = ata(A22)
-    GPU_add(C22, C12, C12, ldc, ldc, ldc, XC2, YC2, 1.0,  1.0);  // C22 = S3 + S4
-    GPU_trans(C11, C22, C22, ldc, ldc, ldc, XC2, YC2, 1.0,  1.0);  // A12_t
-    strassen(A22, W_2, C21, lda, lw2, ldc, XA2, XB2, XC2, YA2, YB2, YC2, depth - 1);  // S5 = strassen(A12_t, A11)
-    GPU_trans(C11, C22, C22, ldc, ldc, ldc, XC2, YC2, 1.0,  1.0);  // A22_t
-    strassen(A12, B21, C11, lda, ldb, ldc, XA2, XB2, XC2, YA2, YB2, YC2, depth - 1);  // S6 = strassen(A22_t, A21)
-    GPU_add(W_1, C11, C11, lw1, ldc, ldc, XC2, YC2, 1.0,  1.0);  // C21 = S5 + S6
+    ata(A11, W_1, lda, lw1, XA2, XA2, YA2, YA2, depth - 1);  // S1 = ata(A11)
+    ata(A21, W_2, lda, lw2, XA2, XA2, YA2, YA2, depth - 1);  // S2 = ata(A21)
+    GPU_add(W_1, W_2, C11, lw1, lw2, ldc, XA2, YA2, 1.0, 1.0);  // C11 = S1 + S2
+    ata(A12, W_1, lda, lw1, XA2, XA2, YA2, YA2, depth - 1);  // S3 = ata(A12)
+    ata(A22, W_2, lda, lw2, XA2, XA2, YA2, YA2, depth - 1);  // S4 = ata(A22)
+    GPU_add(W_1, W_2, C22, lw1, lw2, ldc, XA2, YA2, 1.0,  1.0);  // C22 = S3 + S4
+    GPU_trans(A12, A12_t, C22, lda, lda, XA2, YA2);  // A12_t
+    strassen(A12_t, A11, W_1, lda, lda, lw1, YA2, XA2, XA2, XA2, YA2, YA2, depth - 1);  // S5 = strassen(A12_t, A11)
+    GPU_trans(A22, A22_t, C22, lda, lda, XA2, YA2);  // A22_t
+    strassen(A22_t, A21, W_2, lda, lda, lw2, YA2, XA2, XA2, XA2, YA2, YA2, depth - 1);  // S6 = strassen(A22_t, A21)
+    GPU_add(W_1, W_2, C21, lw1, lw2, ldc, XA2, YA2, 1.0,  1.0);  // C21 = S5 + S6
   }
   cudaFree(W_1);
   cudaFree(W_2);
+  cudaFree(A12_t);
+  cudaFree(A22_t);
 
   /* dynamic peeling fix-up */
   int pxa = XA % 2;
@@ -150,48 +146,38 @@ void ata(double *A, double *C,
 
 
 int main (int argc, char **argv) {
-
-  if(argc != 7) {
-    printf("Usage: strassen <M> <N> <K> <iter> <check> <depth>\n");
+  if(argc != 6) {
+    printf("Usage: strassen <M> <N> <iter> <check> <depth>\n");
     return -1;
   }
 
   int M = atoi(argv[1]);
   int N = atoi(argv[2]);
-  int K = atoi(argv[3]);
-  int iter = atoi(argv[4]);
-  int check = atoi(argv[5]);
-  int depth = atoi(argv[6]);
+  int iter = atoi(argv[3]);
+  int check = atoi(argv[4]);
+  int depth = atoi(argv[5]);
 
-  int sizeA = M * K;
-  int sizeB = K * N;
-  int sizeC = M * N;
+  int sizeA = M * N;
+  int sizeC = M * M;
   int memSizeA = sizeA * sizeof(double);
-  int memSizeB = sizeB * sizeof(double);
   int memSizeC = sizeC * sizeof(double);
 
   double *h_A = (double *)malloc(memSizeA);
-  double *h_B = (double *)malloc(memSizeB);
   double *h_C = (double *)malloc(memSizeC);
   double *v_C = (double *)malloc(memSizeC);
   
-  for (int i = 0; i < sizeA; ++i) {
+  for (int i = 0; i < sizeA; i++) {
     h_A[i] = i % 3;
   }
-  for (int i = 0; i < sizeB; ++i) {
-    h_B[i] = i % 3;
-  }
-  for (int i = 0; i < sizeC; ++i) {
-    h_C[i] = 0.0f;
-    v_C[i] = 0.0f;
+  for (int i = 0; i < sizeC; i++) {
+    h_C[i] = 0.0;
+    v_C[i] = 0.0;
   }
 
-  double *d_A, *d_B, *d_C;
+  double *d_A, *d_C;
   cudaMalloc((void**)&d_A, memSizeA);
-  cudaMalloc((void**)&d_B, memSizeB);
   cudaMalloc((void**)&d_C, memSizeC);
   cudaMemcpy(d_A, h_A, memSizeA, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_B, h_B, memSizeB, cudaMemcpyHostToDevice);
 
   if (cublasCreate(&handle) != CUBLAS_STATUS_SUCCESS) {
     fprintf(stderr, "!!!! CUBLAS initialization error\n");
@@ -201,8 +187,8 @@ int main (int argc, char **argv) {
 
   CudaTimer ct;
   ct.start();
-  for (int i = 0; i < iter; ++i) {
-    strassen(d_A, d_B, d_C, K, N, N, K, N, N, M, K, M, depth);
+  for (int i = 0; i < iter; i++) {
+    ata(d_A, d_C, M, M, M, N, M, M, depth);
   }
   ct.stop();
 
@@ -211,21 +197,21 @@ int main (int argc, char **argv) {
 
 #if 1
   ct.start();
-  for (int i = 0; i < iter; ++i) {
-    verifyByCUBLAS (d_A, d_B, d_C, M, N, K);
+  for (int i = 0; i < iter; i++) {
+    GPU_ata(d_A, d_C, M, N);
   }
   ct.stop();
   
   double classicTime = ct.value() / iter;
-  cudaMemcpy (v_C, d_C, memSizeC, cudaMemcpyDeviceToHost);
+  cudaMemcpy(v_C, d_C, memSizeC, cudaMemcpyDeviceToHost);
 
   double speedup = classicTime / strassenTime;
-  printf ("%d %d %d %.2f %.2f %.2f\n", M, N, K, strassenTime, classicTime, speedup);
+  printf ("%d %d %.2f %.2f %.2f\n", M, N, strassenTime, classicTime, speedup);
 #endif
 
   if (check) {
     double absErr = 0.0;
-    for(int i = 0; i < sizeC; ++i) {
+    for (int i = 0; i < sizeC; i++) {
       absErr += abs(h_C[i] - v_C[i]);
     }
     if (absErr > 1) {
