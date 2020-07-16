51308a998dae21244b0295bc5fc79e04298a88b5
Davide Sforza <sforza.1710262@studenti.uniroma1.it>
2020-07-16 15:29:23 +0200

    Completed base case

--- src/ata.cu
+++ src/ata.cu
@@ -16,7 +16,7 @@ void printm(double* arr_, int m, int n) {
   printf("\n");
 }
 
-void GPU_trans(double *A, double *C,
+void GPU_T(double *A, double *C,
     int lda, int ldc,
     int XA, int YA) {
   double one = 1.0;
@@ -42,17 +42,10 @@ void GPU_AtB(double *A, double *B, double *C,
   cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_T, XB, YA, XA, &alpha, B, ldb, A, lda, &beta, C, ldc);
 }
 
-void GPU_ABt(double *A, double *B, double *C,
-    int lda, int ldb, int ldc,
-    int XA, int XB, int XC,
-    int YA, int YB, int YC,
-    double alpha, double beta) {
-  cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, XB, YA, XA, &alpha, B, ldb, A, lda, &beta, C, ldc);
-}
-
 /*
   lda, ldc is the width in actual memory.
-  XA is the width for computation.
+  XA, XC is the width for computation.
+  Returns the lower triangular part of C.
   A = XA x YA
   C = XC x YC
 */
@@ -73,11 +66,6 @@ void ata(double *A, double *C,
   int lw2 = lw1;
   cudaMalloc((void **)&W_1, lw1 * YC2 * sizeof(double));
   cudaMalloc((void **)&W_2, lw2 * YC2 * sizeof(double));
-  
-  double *A12_t, *A22_t;
-  int lda_t = YA2;
-  cudaMalloc((void **)&A12_t, lda_t * XA2 * sizeof(double));
-  cudaMalloc((void **)&A22_t, lda_t * XA2 * sizeof(double));
 
   int dXA = XA2;
   int dYA = YA2 * lda;
@@ -112,28 +100,38 @@ void ata(double *A, double *C,
 #endif
 
   if (depth <= 1 || stop) {
-    double* A_t;
-    cudaMalloc((void **)&A_t, YA * XA * sizeof(double));
-    GPU_trans(A, A_t, lda, YA, XA, YA);
-    strassen(A_t, A, C, YA, lda, ldc, YA, XA, XC, XA, YA, YC, 1);
+    GPU_AtB(A11, A11, W_1, lda, lda, lw1, XA2, XA2, XC2, YA2, YA2, YC2, 1.0, 0.0);   // S1 = ata(A11)
+    GPU_AtB(A21, A21, W_2, lda, lda, lw2, XA2, XA2, XC2, YA2, YA2, YC2, 1.0, 0.0);  // S2 = ata(A21)
+    GPU_add(W_1, W_2, C11, lw1, lw2, ldc, XC2, YC2, 1.0, 1.0);  // C11 = S1 + S2
+    GPU_AtB(A12, A12, W_1, lda, lda, lw1, XA2, XA2, XC2, YA2, YA2, YC2, 1.0, 0.0);  // S3 = ata(A12)
+    GPU_AtB(A22, A22, W_2, lda, lda, lw2, XA2, XA2, XC2, YA2, YA2, YC2, 1.0, 0.0);  // S4 = ata(A22)
+    GPU_add(W_1, W_2, C22, lw1, lw2, ldc, XC2, YC2, 1.0,  1.0);  // C22 = S3 + S4
+    GPU_AtB(A12, A11, W_1, lda, lda, lw1, XA2, XA2, XC2, YA2, YA2, YC2, 1.0, 0.0);  // S5 = strassen(A12_t, A11)
+    GPU_AtB(A22, A21, W_2, lda, lda, lw2, XA2, XA2, XC2, YA2, YA2, YC2, 1.0, 0.0);  // S6 = strassen(A22_t, A21)
+    GPU_add(W_1, W_2, C21, lw1, lw2, ldc, XC2, YC2, 1.0,  1.0);  // C21 = S5 + S6
   }
   else {
+    double *A2t;
+    int ldt = YA2;
+    cudaMalloc((void **)&A2t, ldt * XA2 * sizeof(double));
+
     ata(A11, W_1, lda, lw1, XA2, XC2, YA2, YC2, depth - 1);  // S1 = ata(A11)
     ata(A21, W_2, lda, lw2, XA2, XC2, YA2, YC2, depth - 1);  // S2 = ata(A21)
     GPU_add(W_1, W_2, C11, lw1, lw2, ldc, XC2, YC2, 1.0, 1.0);  // C11 = S1 + S2
     ata(A12, W_1, lda, lw1, XA2, XC2, YA2, YC2, depth - 1);  // S3 = ata(A12)
     ata(A22, W_2, lda, lw2, XA2, XC2, YA2, YC2, depth - 1);  // S4 = ata(A22)
     GPU_add(W_1, W_2, C22, lw1, lw2, ldc, XC2, YC2, 1.0,  1.0);  // C22 = S3 + S4
-    GPU_trans(A12, A12_t, lda, lda_t, XA2, YA2);  // A12_t
-    strassen(A12_t, A11, W_1, lda_t, lda, lw1, YA2, XA2, XC2, XA2, YA2, YC2, depth - 1);  // S5 = strassen(A12_t, A11)
-    GPU_trans(A22, A22_t, lda, lda_t, XA2, YA2);  // A22_t
-    strassen(A22_t, A21, W_2, lda_t, lda, lw2, YA2, XA2, XC2, XA2, YA2, YC2, depth - 1);  // S6 = strassen(A22_t, A21)
+    GPU_T(A12, A2t, lda, ldt, XA2, YA2);  // A12_t
+    strassen(A2t, A11, W_1, ldt, lda, lw1, YA2, XA2, XC2, XA2, YA2, YC2, depth - 1);  // S5 = strassen(A12_t, A11)
+    GPU_T(A22, A2t, lda, ldt, XA2, YA2);  // A22_t
+    strassen(A2t, A21, W_2, ldt, lda, lw2, YA2, XA2, XC2, XA2, YA2, YC2, depth - 1);  // S6 = strassen(A22_t, A21)
     GPU_add(W_1, W_2, C21, lw1, lw2, ldc, XC2, YC2, 1.0,  1.0);  // C21 = S5 + S6
+    
+    cudaFree(A2t);
+    cudaFree(A2t);
   }
   cudaFree(W_1);
   cudaFree(W_2);
-  cudaFree(A12_t);
-  cudaFree(A22_t);
 
   /* dynamic peeling fix-up */
   int pxa = XA % 2;
